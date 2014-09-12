4f98f381a5269d79626882db90df33376dd5b10a
Dong Mai <dmai@colgate.edu>
2014-09-12 13:34:37 -0400

    Lab01 complete

--- mysimpledb/src/java/simpledb/Catalog.java
+++ mysimpledb/src/java/simpledb/Catalog.java
@@ -133,7 +133,7 @@ public class Catalog {
 
     public Iterator<Integer> tableIdIterator() {
         // some code goes here
-        return null;
+        return idToTable.keySet().iterator();
     }
 
     public String getTableName(int id) {
